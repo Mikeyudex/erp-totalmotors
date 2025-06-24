9f38cf91742a9d835aca8d23e5f0eee3f3d8ea72
Mikeyudex <miguel.yudex@gmail.com>
2025-06-23 19:11:29 -0500

    feat(config): update ecosystem configuration for frontend deployment; specify script, instances, and logging settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,18 @@
 module.exports = {
   apps: [{
-    name: 'erp-totalmotors',
-    script: 'npm',
+    name: 'ERP-TOTALMOTORS-FRONTEND - PORT 3000',
+    script: './node_modules/.bin/next',
     args: 'start',
     cwd: '/app',
+    instances: 1,
+    exec_mode: 'fork',
     env: {
       NODE_ENV: 'production',
       PORT: 3000
-    }
+    },
+    error_file: '/app/logs/err.log',
+    out_file: '/app/logs/out.log',
+    log_file: '/app/logs/combined.log',
+    time: true
   }]
 }
